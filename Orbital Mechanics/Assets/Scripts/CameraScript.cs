f3588fd8a05643240c0aaa665d14c4af1a14461c
MrBoogyBam <MrBoogyBamBusiness@gmail.com>
2024-07-27 05:32:52 +0300

    Improved camera controls

--- Orbital Mechanics/Assets/Scripts/CameraScript.cs
+++ Orbital Mechanics/Assets/Scripts/CameraScript.cs
@@ -5,20 +5,34 @@ public class CameraScript : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private float distance;
     [SerializeField] private float smoothSpeed;
+    [SerializeField] private float smoothZoomSpeed;
+    [SerializeField] private float smoothSwitchSpeed;
 
     [Header("Camera Control")]
     [SerializeField] private float zoomAmount;
     [SerializeField] private float sensitivity;
+    [SerializeField] private float maxDistance;
+
+    private CameraVariables selectedPlanet;
+
+    private Transform oldTarget;
 
     private Vector3 smoothPosition;
+    private Vector3 virtualTargetPosition;
 
+    private Quaternion rotation;
     private Quaternion smoothRotation;
 
+    private float desiredDistance;
     private float xRotation;
     private float yRotation;
 
     private void Start()
     {
+        oldTarget = target;
+        desiredDistance = distance;
+        selectedPlanet = target.GetComponent<CameraVariables>();
+
         Vector3 angles = transform.eulerAngles;
 
         xRotation = angles.x;
@@ -27,7 +41,11 @@ public class CameraScript : MonoBehaviour
 
     private void Update()
     {
-        smoothPosition = smoothRotation * new Vector3(0, 0, -distance) + target.position;
+        distance = Mathf.Lerp(distance, desiredDistance, smoothZoomSpeed);
+        Debug.Log(Vector3.Distance(virtualTargetPosition, target.position));
+        virtualTargetPosition = Vector3.Distance(virtualTargetPosition, target.position) > 5 ? Vector3.Lerp(virtualTargetPosition, target.position, smoothSwitchSpeed) : target.position;
+        smoothPosition = smoothRotation * new Vector3(0, 0, -distance) + virtualTargetPosition;
+        smoothRotation = Quaternion.Lerp(transform.rotation, rotation, smoothSpeed);
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -36,21 +54,23 @@ public class CameraScript : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider != null)
+                if (hit.collider != null && hit.transform != target)
                 {
+                    oldTarget = target;
                     target = hit.transform;
+
+                    selectedPlanet = target.GetComponent<CameraVariables>();
+
+                    desiredDistance = Mathf.Clamp(desiredDistance, selectedPlanet.minDistance, maxDistance);
                 }
             }
         }
 
         if(Input.mouseScrollDelta.y != 0)
         {
-            distance += distance <= 400 ? -Input.mouseScrollDelta.y * (zoomAmount / 5) : -Input.mouseScrollDelta.y * zoomAmount;
-            if (distance < 50)
-            {
-                distance = 50;
-                return;
-            }
+            desiredDistance += desiredDistance <= selectedPlanet.slowDownDistance ? -Input.mouseScrollDelta.y * (zoomAmount / selectedPlanet.slowDownAmount) : -Input.mouseScrollDelta.y * zoomAmount;
+
+            desiredDistance = Mathf.Clamp(desiredDistance, selectedPlanet.minDistance, maxDistance);
         }
 
         if(Input.GetMouseButton(1))
@@ -60,9 +80,7 @@ public class CameraScript : MonoBehaviour
 
             yRotation = Mathf.Clamp(yRotation, -90, 90);
 
-            Quaternion rotation = Quaternion.Euler(yRotation, xRotation, 0);
-            smoothRotation = Quaternion.Lerp(transform.rotation, rotation, smoothSpeed);
-            smoothPosition = smoothRotation * new Vector3(0, 0, -distance) + target.position;
+            rotation = Quaternion.Euler(yRotation, xRotation, 0);
         }
     }
 
---
+++ Orbital Mechanics/Assets/Scripts/CameraVariables.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class CameraVariables : MonoBehaviour
+{
+    public float minDistance;
+    public float slowDownDistance;
+    public float slowDownAmount;
+}
